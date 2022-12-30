0088c2dec131063e18739fbf711546c6a335eb3c
kmkim <kim.kyungmin00@gmail.com>
2022-12-30 15:23:30 +0900

    init

--- src/script_var_selection_data.sh
+++ src/script_var_selection_data.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# open with binary. make sure file format is unix e.g vim -b filename
-# :%s/^M//g (^M=Ctrl+v+m)
-
-s_str='var_select_factor = 0.1'
-t_str='var_select_factor = 0.2'
-sed -ri 's/$s_str $t_str/' ./header/RUNHEADER.py
-
+sed -ri 's/var_select_factor = 0.1/var_select_factor = 0.2/' ./header/index_forecasting/RUNHEADER.py
 python script_index_forecasting_generate_data.py --e_test="2010-01-01" --dataset_version="v0" --m_target_index=8 --gen_var=0 --domain=None
