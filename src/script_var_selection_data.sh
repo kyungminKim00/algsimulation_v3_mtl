de1b1cb46e7a06565354d58a2caf00845274f66b
kmkim <kim.kyungmin00@gmail.com>
2022-12-30 16:21:38 +0900

    init

--- src/script_var_selection_data.sh
+++ src/script_var_selection_data.sh
@@ -1,2 +1,4 @@
-sed -ri 's/var_select_factor = 0.2/var_select_factor = 0.3/' ./header/index_forecasting/RUNHEADER.py
+#!/bin/bash
+
+sed -ri 's/var_select_factor = 0.3/var_select_factor = 0.5/' ./header/index_forecasting/RUNHEADER.py
 python script_index_forecasting_generate_data.py --e_test="2010-01-01" --dataset_version="v0" --m_target_index=8 --gen_var=0 --domain=None
